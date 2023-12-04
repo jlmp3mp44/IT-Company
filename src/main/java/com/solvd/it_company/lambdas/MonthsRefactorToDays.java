e747d350632ed962124fb12ba0da44c036755a17
Julya <julya.meleshko@gmail.com>
2023-12-04 20:03:36 +0200

    Change

--- src/main/java/com/solvd/it_company/lambdas/MonthsRefactorToDays.java
+++ src/main/java/com/solvd/it_company/lambdas/MonthsRefactorToDays.java
@@ -1,5 +1,6 @@
 package com.solvd.it_company.lambdas;
+
 @FunctionalInterface
-public interface MonthsRefactorToDays<T extends Number, R extends Number, S extends Number> {
+public interface MonthsRefactorToDays<T, R, S> {
     R monthsToDays(T months, S days);
 }
